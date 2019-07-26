32beb55779125bdd925e2e0b7f03e23c09956b8a
Joeyu <xiaojiong@pinduoduo.com>
2019-07-26 15:26:12 -0400

    new

--- StackAndQueue/stack.py
+++ StackAndQueue/stack.py
@@ -1,6 +1,10 @@
 from linkedlist import LNode
+from linkedlist import LList
 
 class StackUnderFlow(ValueError):
+    pass
+
+class QueueUnderFlow(ValueError):
     pass
 
 class SStack:
@@ -49,86 +53,46 @@ class LStack:
         self._top = self._top.next
         return p
 
-def check_parens(text):
-    parents = "()[]{}"
-    open_parens = "([{"
-    opposite = {"(":")", "[":"]", "{":"}"}
+class LQueue(LList):
 
-def knap_rec_r(weight_goal, wlist, n):
-    if weight_goal == 0:
-        return True
-    if knap_rec_r(weight_goal - wlist[n-1], wlist, n-1):
-        return 
+    def __init__(self):
+        LList.__init__(self)
+        self._rear = None
 
-def knap_rec(weight_goal, wlist, n):
+    def enqueue(self, elem):
+        if self._rear is None:
+            self._head = LNode(elem, self._head)
+            self._rear = self._head
+        else:
+            self._rear.next = LNode(elem)
+            self._rear = self._rear.next
 
-    stack = SStack()
-    current_weight = 0
-    while len(wlist) > 0:
-        current_weight == 0
-        for i in range(len(wlist)):
-            print(wlist[i:])
-            print("-----------------------")
-            current_weight += wlist[i]
-            if current_weight < weight_goal:
-                stack.push(wlist[i])
-            elif current_weight == weight_goal:
-                stack.push(wlist[i])
-                while not stack.is_empty():
-                    print(str(stack.pop()))
-                return
-            else:
-                if stack.is_empty():
-                    break
-                e = stack.pop()
-                if stack.is_empty():
-                    wlist.remove(e)
-                    break
-                else:
-                    current_weight -= e
-                    continue
+    def is_empty(self):
+        return self._head is None
 
-    #
-    #
-    #
-    #
-    #
-    #
-    #
-    # # stack_info = SStack()
-    #
-    # weight_tol = 0
-    # for i in wlist:
-    #     stack_info.push(i)
-    #     weight_tol += i
-    #
-    # while not stack_info.is_empty() or not stack.is_empty():
-    #     if weight_tol == weight_goal:
-    #         return True
-    #     elif weight_tol < weight_goal:
-    #
-    #     else:
-    #         weight_top = stack_info.pop()
-    #         weight_tol = weight_tol - weight_top
-    #         if weight_goal == weight_tol - weight_top:
-    #             return True
-    #         elif weight_goal < weight_tol:
-    #             continue
-    #         else:
-    #
-    #
-    #
-    #
-    # while len(wlist) >= 0
-    #     weight += i
-    #     if weight_goal > weight:
-    #         stack.push(i)
-    #     elif weight_goal = weight:
-    #         print(i)
-    #         while not stack.is_empty():
-    #             print(stack.pop())
-    #     else:
-    #         stack.pop()
+    def dequeue(self):
+        if self._rear is None:
+            raise QueueUnderFlow("in dequeue")
+        else:
+            e = self._head.elem
+            self._head = self._head.next
+        return e
+
+    def peek(self):
+        if self._head is None:
+            raise QueueUnderFlow("in peeking")
+        else:
+            return self._head.elem
+
+class SQueue:
+    g
+
+
+
+
+
+
+
 
 
 
