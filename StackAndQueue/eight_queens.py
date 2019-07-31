65527f658ae6767af864c4df8a5d15494b8cfab8
Joey Zhou <woshizhouyia@gmail.com>
2019-07-30 21:54:49 -0400

    Update eight_queens.py

--- StackAndQueue/eight_queens.py
+++ StackAndQueue/eight_queens.py
@@ -38,6 +38,9 @@ class chessBoard:
     def __init__(self, board_x = 8, board_y = 8):
         self.board_x = board_x
         self.board_y = board_y
+        self.make_board()
+
+    def make_board(self):
         bd = []
         for i in range(self.board_x):
             bd_1 = []
@@ -76,19 +79,19 @@ class chessBoard:
             new_pos = (x, y)
         return new_pos
 
+    @staticmethod
+    def validate_pos(pos):
+        if pos[0] < 8 and pos[0] >= 0 and pos[1] < 8 and pos[0] >= 0:
+            return True
+        else:
+            return False
 
     def occupy_next_pos(self, pos, occupy = True):
-
-       def validate_pos(pos):
-            if pos[0] < 8 and pos[0] >= 0 and pos[1] < 8 and pos[0] >= 0:
-                return True
-            else:
-                return False
 
        new_pos = pos
        while True:
            new_pos = chessBoard.get_next_pos(new_pos)
-           if not validate_pos(new_pos):
+           if not chessBoard.validate_pos(new_pos):
                return (-1, -1)
            elif not self.is_occupy(new_pos) and occupy:
                self.board[pos[0]][pos[1]] = 1
@@ -108,34 +111,24 @@ class chessBoard:
 
 if __name__ == "__main__":
 
-    def validate_pos(pos):
-        if pos[0] < 8 and pos[0] >= 0 and pos[1] < 8 and pos[0] >= 0:
-            return True
-        else:
-            return False
-
     newboard = chessBoard()
-    stack = SStack()
     queens_list = []
 
     for i in range(8):
         for j in range(8):
-            if len(queens_list) < 8:
+            if len(queens_list) < 9:
                 if not newboard.is_occupy((i,j)):
                     newboard.put_queen(i,j)
-                    stack.push((i,j))
                     queens_list.append((i, j))
     print(queens_list)
 
     while True:
         if len(queens_list) < 9:
             # print(queens_list)
-            latest_pos = stack.pop()
-            queens_list.pop()
+            latest_pos = queens_list.pop()
             if len(queens_list) == 0:
                 init_queen = chessBoard.get_next_pos(latest_pos)
-                if validate_pos(init_queen):
-                    stack.push(init_queen)
+                if chessBoard.validate_pos(init_queen):
                     queens_list.append(init_queen)
                 else:
                     break
@@ -143,13 +136,12 @@ if __name__ == "__main__":
             for k in queens_list:
                 newboard.put_queen(k[0], k[1])
             next_pos = newboard.occupy_next_pos(latest_pos, False)
-            while validate_pos(next_pos) and len(queens_list) < 9:
-                if not validate_pos(next_pos):
+            while chessBoard.validate_pos(next_pos) and len(queens_list) < 9:
+                if not chessBoard.validate_pos(next_pos):
                     break
                 else:
                     new_queen = queens(next_pos)
                     queens_list.append(next_pos)
-                    stack.push(next_pos)
                     newboard.put_queen(next_pos[0], next_pos[1])
                     next_pos = newboard.occupy_next_pos(next_pos, occupy = False)
             if len(queens_list) == 8:
