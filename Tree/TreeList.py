605c8024bdf59dff03e2fe213cae558518d399ae
joey.zhou <woshizhouyia@gmail.com>
2022-03-19 16:35:43 -0400

    added tree data structure

---
+++ Tree/TreeList.py
@@ -0,0 +1,81 @@
+import traversal as traversal
+
+from treeABC import BinTreeABC
+
+
+class BinTreeList(BinTreeABC):
+
+    def __init__(self, data, left=None, right=None):
+        if left is not None:
+            assert isinstance(left, BinTreeList)
+        if right is not None:
+            assert isinstance(right, BinTreeList)
+        self.root = [data, left, right]
+
+    def is_empty(self):
+        if self.root[0] is None:
+            return True
+        else:
+            return False
+
+    def data(self):
+        return self.root[0]
+
+    def num_nodes(self):
+        if self.root[0] is None:
+            return 0
+        elif self.root[0] is not None:
+            return 1
+        else:
+            return self.left().num_nodes() + 1 + self.right().num_nodes()
+
+    def left(self):
+        return self.root[1]
+
+    def right(self):
+        return self.root[2]
+
+    def set_root(self, data):
+        self.root[0] = data
+
+    def set_left(self, tree):
+        self.root[2] = tree
+
+    def set_right(self, tree):
+        self.root[2] = tree
+
+    def traversal(self):
+        if self.is_empty() is True:
+            return
+        if self.left() is None and self.right() is None:
+            print(self.data())
+        if self.left() is not None:
+            self.left().traversal()
+            print(self.data())
+        if self.right() is not None:
+            self.right().traversal()
+
+    def forall(self, op):
+        if self.is_empty() is True:
+            return
+        if self.left() is None and self.right() is None:
+            self.set_root(op(self.data()))
+        if self.left() is not None:
+            self.left().forall(op)
+            self.set_root(op(self.data()))
+        if self.right() is not None:
+            self.right().forall(op)
+
+    def __str__(self):
+        return ' '.join(map(str, self.root))
+
+
+if __name__ == "__main__":
+    t1 = BinTreeList(2, BinTreeList(4), None)
+    t2 = BinTreeList(8, BinTreeList(6), BinTreeList(20))
+    t3 = BinTreeList(5, t1, t2)
+    print(t3)
+    t3.traversal()
+    t3.forall(lambda x: x**2)
+    print(t3)
+
---
+++ Tree/treeABC.py
@@ -0,0 +1,40 @@
+from abc import abstractmethod, ABC
+
+
+class BinTreeABC(ABC):
+
+    @abstractmethod
+    def is_empty(self):
+        pass
+
+    @abstractmethod
+    def num_nodes(self):
+        pass
+
+    @abstractmethod
+    def data(self):
+        pass
+
+    @abstractmethod
+    def left(self):
+        pass
+
+    @abstractmethod
+    def right(self):
+        pass
+
+    @abstractmethod
+    def set_left(self, btree):
+        pass
+
+    @abstractmethod
+    def set_right(self, btree):
+        pass
+
+    @abstractmethod
+    def traversal(self):
+        pass
+
+    @abstractmethod
+    def forall(self, op):
+        pass
