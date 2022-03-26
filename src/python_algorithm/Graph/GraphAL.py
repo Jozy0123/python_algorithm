4da56cd85d12eab5f319e0468cf7f3fa7bfe9c0a
joeyzhou <woshizhouyia@gmail.com>
2022-03-26 15:15:19 -0400

    bug fix

--- src/python_algorithm/Graph/GraphAL.py
+++ src/python_algorithm/Graph/GraphAL.py
@@ -13,13 +13,14 @@ def out_edge(matrix_row: List[int],
     return edge_list
 
 
-## undirectional graph
+## for both directional and undirectional graph
 class GraphAL(GraphABC):
 
     def __init__(self,
                  matrix: Optional[List[List[int]]] = None,
                  unconn: Union[int, float] = 0,
-                 nodes_map: Optional[Dict[int, int]] = None
+                 nodes_map: Optional[Dict[int, int]] = None,
+                 directional: bool = False
                  ):
         if nodes_map is None:
             nodes_map = {i + 1: i for i in range(len(matrix))}
@@ -40,6 +41,7 @@ class GraphAL(GraphABC):
         self._vnum = vertices_num
         self._unconn = unconn
         self._nodes_map = nodes_map
+        self._directional = directional
 
     def is_empty(self):
         return self._vnum == 0
@@ -48,15 +50,23 @@ class GraphAL(GraphABC):
         return self._vnum
 
     def edge_num(self):
-        return sum([len(i) for i in self._matrix])
+        if self._directional:
+            return sum([len(i) for i in self._matrix])
+        else:
+            return sum([len(i) for i in self._matrix]) / 2
 
     def vertices(self):
         return list(self._nodes_map.keys())
 
     def edges(self):
-        for i in self._matrix:
-            for j in i:
-                yield j
+        if self._directional:
+            for i in self._nodes_map.keys():
+                for j in self._matrix[self._nodes_map[i]]:
+                    yield f"{j[0]} -> {i}"
+        else:
+            for i in self._nodes_map.keys():
+                for j in self._matrix[self._nodes_map[i]][i-1:]:
+                    yield f"{j[0]} <-> {i}"
 
     def add_vertex(self, v):
         if self._nodes_map.get(v, None) is not None:
@@ -68,10 +78,17 @@ class GraphAL(GraphABC):
     def add_edge(self, v1, v2):
         if self._nodes_map.get(v1, None) is None or self._nodes_map.get(v2, None) is None:
             raise ValueError("at least one of the node does not exist, add it first")
-        for i in self._matrix[self._nodes_map[v1]]:
-            if v2 == i[0]:
-                raise ValueError("edge exists!")
-        self._matrix[self._nodes_map[v1]].insert(self._nodes_map[v2], (v2, 1))
+        if not self._directional:
+            for i in self._matrix[self._nodes_map[v1]]:
+                if v2 == i[0]:
+                    raise ValueError("edge exists!")
+            self._matrix[self._nodes_map[v1]].insert(self._nodes_map[v2], (v2, 1))
+            self._matrix[self._nodes_map[v2]].insert(self._nodes_map[v1], (v1, 1))
+        else:
+            for i in self._matrix[self._nodes_map[v1]]:
+                if v2 == i[0]:
+                    raise ValueError("edge exists!")
+            self._matrix[self._nodes_map[v1]].insert(self._nodes_map[v2], (v2, 1))
 
     def get_edge(self, v1, v2):
         if self._nodes_map.get(v1, None) is None or self._nodes_map.get(v2, None) is None:
@@ -80,6 +97,7 @@ class GraphAL(GraphABC):
             if v2 == i[0]:
                 return v1, v2, i[1]
         return v1, v2, 0
+
 
     def out_edge(self, v):
         for i in self._matrix[self._nodes_map[v]]:
