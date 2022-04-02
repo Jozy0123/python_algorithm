f2c66bcd16270dfeef453d8f37c303a02e323ad0
joeyzhou <woshizhouyia@gmail.com>
2022-04-02 18:16:52 -0400

    add quick sort

--- src/python_algorithm/Sort/quick_sort.py
+++ src/python_algorithm/Sort/quick_sort.py
@@ -2,10 +2,6 @@ from python_algorithm.Sort.record import Record
 from typing import List
 
 
-def quick_sort(lst: List[Record]) -> List[Record]:
-    quick_sort_recursion(lst, 0, len(lst)-1)
-
-
 def quick_sort_recursion(lst, start, end):
     if start >= end:
         return
@@ -17,33 +13,39 @@ def quick_sort_recursion(lst, start, end):
     empty_lot = small_list_start
 
     is_pointer_front = False
-    while small_list_start < large_list_start:
-        if is_pointer_front:
-            if lst[small_list_start] <= pivot:
-                small_list_start += 1
-                continue
-            elif lst[small_list_start] > pivot:
-                lst[empty_lot] = lst[small_list_start]
-                empty_lot = small_list_start
-                small_list_start += 1
-                is_pointer_front = False
-                continue
+    while small_list_start <= large_list_start:
+
+        if is_pointer_front and lst[small_list_start] <= pivot:
+            small_list_start += 1
+
+        elif is_pointer_front and lst[small_list_start] > pivot:
+            lst[empty_lot] = lst[small_list_start]
+            empty_lot = small_list_start
+            small_list_start += 1
+            is_pointer_front = False
+
+        elif is_pointer_front is False and lst[large_list_start] > pivot:
+            large_list_start -= 1
+
         else:
-            if lst[large_list_start] >= pivot:
-                large_list_start -= 1
-                continue
-            elif lst[large_list_start] < pivot:
-                lst[empty_lot] = lst[large_list_start]
-                empty_lot = large_list_start
-                large_list_start -= 1
-                is_pointer_front = True
-                continue
+            lst[empty_lot] = lst[large_list_start]
+            empty_lot = large_list_start
+            large_list_start -= 1
+            is_pointer_front = True
 
-    lst[empty_lot] = pivot
-    quick_sort_recursion(lst, start, empty_lot-1)
-    quick_sort_recursion(lst, empty_lot+1, end)
+    if empty_lot != start:
+        lst[empty_lot] = pivot
+
+    quick_sort_recursion(lst, start, empty_lot - 1)
+    quick_sort_recursion(lst, empty_lot + 1, end)
 
 
+def quick_sort(lt: List[Record]):
+    quick_sort_recursion(lt, 0, len(lt) - 1)
+
+
+# Driver code to test above
 if __name__ == "__main__":
-    lst = [8, 9, 1, 7, 6, 8, 20]
-    quick_sort(lst)
+    test = [8, 9, 1, 7, 6, 8, 20, 8, 5, 7, 6, 7, 8, 11, 20, 60, 55, 42, 35, 66, 5, 4]
+    quick_sort(test)
+    print(test)
